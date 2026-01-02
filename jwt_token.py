3076165e7545d5c7ae3ffd77ffd93a62fa8510f2
princeSinglaCoder <151184684+princeSinglacoder@users.noreply.github.com>
2026-01-02 12:49:42 +0530

    Update jwt_token.py

--- jwt_token.py
+++ jwt_token.py
@@ -8,5 +8,5 @@ def create_token(data):
     data["exp"] = datetime.utcnow() + timedelta(hours=3)
     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
 
-def decode_token(token):
+def decode_token(token):fjhkswjekefh;ioi0[w'
     return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
